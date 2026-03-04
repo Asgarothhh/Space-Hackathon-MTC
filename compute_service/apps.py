a02e8750d98d4780aa312b696b3757b1e23606cd
Artem Kavalchuk <artemkovalcuk301@gmail.com>
2026-03-04 12:36:05 +0300

    Deleted django base

--- compute_service/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComputeServicConfig(AppConfig):
-    name = 'compute_service'
--- orchestrator/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Job
-
-class JobSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Job
-        fields = '__all__'
-        read_only_fields = ('status','started_at','finished_at','attempts','error_message')
